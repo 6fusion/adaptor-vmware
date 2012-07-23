a56dac04893e1d1741a23b0b5414a3cc5807650a
dseymour@6fusion.com <dseymour@6fusion.com>
2012-07-22 22:47:12 -0400

    - Refactored the machine readings to improve performance

--- models/machine_disk.rb
+++ models/machine_disk.rb
@@ -1,6 +1,9 @@
 # @api public
 class MachineDisk < Base::MachineDisk
-  attr_accessor :vm,:key
+  attr_accessor :vm,
+                :stats,
+                :key
+
   # This is where you would call your cloud service and
   # find a specific machine's disk's readings.
   # This request should support since (start_date) and until (end_date)
@@ -9,40 +12,30 @@ class MachineDisk < Base::MachineDisk
   # @param [Time] _since The beginning date/time for the requested readings
   # @param [Time] _until The ending date/time for the requested readings
   # @return [Machine]
-  def readings(i_node, _since = Time.now.utc - 86400, _until = Time.now.utc)
+  def readings(i_node, _since = Time.now.utc - 1800, _until = Time.now.utc)
     logger.info('machine_disk.readings')
 
-    vim = RbVmomi::VIM.connect :host => i_node.connection, :user => i_node.credentials_hash["username"], :password => i_node.credentials_hash["password"] , :insecure => true
-    pm = vim.serviceContent.perfManager
-    vms = [vm]
-    metrics = {"virtualDisk.read.average" => "*","virtualDisk.write.average" => "*"}
+    #Create machine disk readings
+    readings_from_stats(stats)
+  end
 
-    # Collects Performance information
-    vm.stats = pm.retrieve_stats(vms,metrics,300,_since,_until)
-
-    readings = Array.new
-    stats.each do |p|
-      if p.entity == self.vm
-        for f in 0..p.sampleInfo.length - 1
-          if p.value.empty?
-            reading = MachineDiskReading.new(
-                usage: 32,
-                read:  0,
-                write: 0
-            )
-          else
-            reading = MachineDiskReading.new(
-                usage: 32,
-                read:  p.value[0].value[f].to_s,
-                write: p.value[1].value[f].to_s
-            )
-          end
-
-          readings << reading
-        end
+  def readings_from_stats(performance_metrics)
+    # Helper Method for creating readings objects.
+    performance_metrics.sampleInfo.each_with_index.map do |x,i|
+      if performance_metrics.value.empty?
+        MachineDiskReading.new(
+            usage: 32,
+            read:  0,
+            write: 0
+        )
+      else
+        metric_readings = Hash[performance_metrics.value.map{|s| ["#{s.id.counterId}.#{s.id.instance}",s.value]}]
+        MachineDiskReading.new(
+            usage: 32,
+            read:  metric_readings["173.scsi0:0"][i].to_s,
+            write: metric_readings["174.scsi0:0"][i].to_s
+        )
       end
     end
-
-    readings
   end
 end
