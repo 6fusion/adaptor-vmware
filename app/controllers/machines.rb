ad5fdd2f5502fd68fe8c8ba6f5d6eda5a8b00772
dseymour@6fusion.com <dseymour@6fusion.com>
2012-07-24 15:28:33 -0400

    Refactored the vCenter session management

--- app/controllers/machines.rb
+++ app/controllers/machines.rb
@@ -1,86 +1,136 @@
 AdaptorVMware.controllers :machines, :map => "/inodes/:inode_uuid" do
   before do
     @i_node = INode.find_by_uuid(params[:inode_uuid])
-    @i_node.open_session
-  end
-
-  after do
-    @i_node.close_session
   end
 
   # Creates
   post :create do
-    @machine = Machine.new(params)
-    @machine.save(@i_node)
+    begin
+      @i_node.open_session
+      @machine = Machine.new(params)
+      @machine.save(@i_node)
 
-    render 'machines/show'
+      render 'machines/show'
+    ensure
+      @i_node.close_session
+    end
   end
 
   # Reads
   get :index do
-    @machines = Machine.all(@i_node)
+    begin
+      @i_node.open_session
+      @machines = Machine.all(@i_node)
 
-    render 'machines/index'
+      render 'machines/index'
+    ensure
+      @i_node.close_session
+    end
   end
 
   get :index, :map => 'machines/readings' do
-    @machines = Machine.all_with_readings(@i_node)
+    begin
+      @i_node.open_session
+      @machines = Machine.all_with_readings(@i_node)
 
-    render 'machines/readings'
+      render 'machines/readings'
+    ensure
+      @i_node.close_session
+    end
   end
 
   get :show, :map => "machines/:uuid" do
-    @machine = Machine.find_by_uuid(@i_node, params[:uuid])
+    begin
+      @i_node.open_session
+      @machine = Machine.find_by_uuid(@i_node, params[:uuid])
 
-    render 'machines/show'
+      render 'machines/show'
+    ensure
+      @i_node.close_session
+    end
   end
   get :index, :map => 'machines/:uuid/readings' do
-    @machine = Machine.find_by_uuid_with_readings(@i_node, params[:uuid])
+    begin
+      @i_node.open_session
+      @machine = Machine.find_by_uuid_with_readings(@i_node, params[:uuid])
 
-    render 'machines/readings'
+      render 'machines/readings'
+    ensure
+      @i_node.close_session
+    end
   end
 
   # Updates
   put :show, :map => 'machines/:uuid/power_on' do
-    @machine = Machine.find_by_uuid(@i_node, params[:uuid])
-    @machine.power_on(@i_node) if @machine.present?
+    begin
+      @i_node.open_session
+      @machine = Machine.find_by_uuid(@i_node, params[:uuid])
+      @machine.power_on(@i_node) if @machine.present?
 
-    render 'machines/show'
+      render 'machines/show'
+    ensure
+      @i_node.close_session
+    end
   end
 
   put :show, :map => 'machines/:uuid/power_off' do
-    @machine = Machine.find_by_uuid(@i_node, params[:uuid])
-    @machine.power_off(@i_node) if @machine.present?
+    begin
+      @i_node.open_session
+      @machine = Machine.find_by_uuid(@i_node, params[:uuid])
+      @machine.power_off(@i_node) if @machine.present?
 
-    render 'machines/show'
+      render 'machines/show'
+    ensure
+      @i_node.close_session
+    end
   end
 
   put :show, :map => 'machines/:uuid/restart' do
-    @machine = Machine.find_by_uuid(@i_node, params[:uuid])
-    @machine.restart(@i_node) if @machine.present?
+    begin
+      @i_node.open_session
+      @machine = Machine.find_by_uuid(@i_node, params[:uuid])
+      @machine.restart(@i_node) if @machine.present?
 
-    render 'machines/show'
+      render 'machines/show'
+    ensure
+      @i_node.close_session
+    end
   end
 
   put :show, :map => 'machines/:uuid/shutdown' do
-    @machine = Machine.find_by_uuid(@i_node, params[:uuid])
-    @machine.shutdown(@i_node) if @machine.present?
+    begin
+      @i_node.open_session
+      @machine = Machine.find_by_uuid(@i_node, params[:uuid])
+      @machine.shutdown(@i_node) if @machine.present?
 
-    render 'machines/show'
+      render 'machines/show'
+    ensure
+      @i_node.close_session
+    end
   end
 
   put :show, :map => 'machines/:uuid/unplug' do
-    @machine = Machine.find_by_uuid(@i_node, params[:uuid])
-    @machine.unplug(@i_node) if @machine.present?
+    begin
+      @i_node.open_session
+      @machine = Machine.find_by_uuid(@i_node, params[:uuid])
+      @machine.unplug(@i_node) if @machine.present?
 
-    render 'machines/show'
+      render 'machines/show'
+    ensure
+      @i_node.close_session
+    end
   end
 
   # Deletes
   delete :delete, :map => "machines/:uuid" do
-    @machine = Machine.find_by_uuid(@i_node, params[:uuid])
-    @machine.delete(@i_node)
+    begin
+      @i_node.open_session
+      @machine = Machine.find_by_uuid(@i_node, params[:uuid])
+      @machine.delete(@i_node)
 
-    render 'machines/show'
+      render 'machines/show'
+    ensure
+      @i_node.close_session
+    end
   end
 end
