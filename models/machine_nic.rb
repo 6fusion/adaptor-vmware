04d81cad825a74ec3d3fd79e21de99338b07ca44
Delano Seymour <dseymour@6fusion.com>
2012-08-14 00:56:40 -0400

    Added Date/Time to Disk and NIC readings

--- models/machine_nic.rb
+++ models/machine_nic.rb
@@ -21,6 +21,7 @@ class MachineNic < Base::MachineNic
       else
         metric_readings = Hash[performance_metrics.value.map{|s| ["#{s.id.counterId}.#{s.id.instance}",s.value]}]
         MachineNicReading.new(
+            date_time:  x.timestamp.to_s,
             receive:    metric_readings["148.#{key}"].nil? ? 0 : metric_readings["148.#{key}"][i] == -1 ? 0 : metric_readings["148.#{key}"][i].to_s,
             transmit:   metric_readings["149.#{key}"].nil? ? 0 : metric_readings["149.#{key}"][i] == -1 ? 0 : metric_readings["149.#{key}"][i].to_s
         )
