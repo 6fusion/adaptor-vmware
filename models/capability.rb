f661786ac0d25e91db129e3b7b4e684a747e358f
Geoff Corey <gcorey@6fusion.com>
2012-10-01 07:26:51 -0700

    Added machine_readings capability

--- models/capability.rb
+++ models/capability.rb
@@ -1,6 +1,18 @@
 class Capability < Base::Capability
 
-  SUPPORTED_CAPABILITIES = %w(machines machines_readings machine start stop restart force_stop force_restart delete)
+  SUPPORTED_CAPABILITIES = %w(
+    inode 
+    machines 
+    machines_readings 
+    machine 
+    machine_readings 
+    start 
+    stop 
+    restart 
+    force_stop 
+    force_restart 
+    delete
+    )
 
   def self.all(inode)
     logger.info('Capability.all')
