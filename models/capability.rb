b1dcdaad51e27c3f8abbc81e32d90f88b46fab2f
Geoff Corey <gcorey@6fusion.com>
2012-11-27 16:19:36 +0000

    modified capabilities

--- models/capability.rb
+++ models/capability.rb
@@ -1,19 +1,24 @@
 class Capability < Base::Capability
 
+  # SUPPORTED_CAPABILITIES = %w(
+  #   inode 
+  #   machines 
+  #   machines_readings 
+  #   machine 
+  #   machine_readings 
+  #   start 
+  #   stop 
+  #   restart 
+  #   force_stop 
+  #   force_restart 
+  #   delete
+  #   )
   SUPPORTED_CAPABILITIES = %w(
-    inode 
     machines 
     machines_readings 
     machine 
     machine_readings 
-    start 
-    stop 
-    restart 
-    force_stop 
-    force_restart 
-    delete
     )
-
   def self.all(inode)
     logger.info('Capability.all')
     SUPPORTED_CAPABILITIES.map do |capability|
