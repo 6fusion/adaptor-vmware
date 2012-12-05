e3f12da2076a05db16029e93292ec2e15867e0da
Geoff Corey <gcorey@6fusion.com>
2012-12-05 04:44:36 +0000

    Added proxy support to the hypervisor

--- models/inode.rb
+++ models/inode.rb
@@ -1,11 +1,21 @@
+
+require 'lib/unix_proxy_environment'
+
 class INode < Base::INode
   attr_reader :uuid, :session, :host_ip_address, :user, :password
 
   def open_session
     begin
       # Connect to vCenter if the session is not already established
-      logger.info("INode.open_session")
-      @session ||= RbVmomi::VIM.connect :host => @host_ip_address, :user => @user, :password => @password, :insecure => true
+      logger.info("INode.open_session")        
+      proxy_uri = SixFusion::find_proxy('http_proxy',@host_ip_address)
+      if proxy_uri
+        logger.info("RvVmomi proxy used "+proxy_uri.host.to_s+":"+proxy_uri.port.to_s)
+        @session ||= RbVmomi::VIM.connect(:host => @host_ip_address, :user => @user, :password => @password, :insecure => true, :proxyHost => proxy_uri.host, :proxyPort => proxy_uri.port)
+      else
+        logger.info("RbVmomi no proxy used")
+        @session ||= RbVmomi::VIM.connect(:host => @host_ip_address, :user => @user, :password => @password, :insecure => true)
+      end
       if (Time.now.utc - @session.serviceInstance.CurrentTime).abs > 300
         raise Exceptions::Forbidden.new("Local time is more than 5 minute out of sync with the hypervisor's time. Local time is #{Time.now.utc} and the hypervisor's time is #{@session.serviceInstance.CurrentTime}.")
       end
