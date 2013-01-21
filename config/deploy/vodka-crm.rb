57a7a9499b8d94321b6e9e1ba0c730125c11bbc8
Chris Altman <chris@altonymous.com>
2013-01-21 15:30:40 -0500

    Add whiskey/vodka deployment stages

---
+++ config/deploy/vodka-crm.rb
@@ -0,0 +1,4 @@
+set :deploy_env, "vodka-crm"
+set :context_path, "/vmware"
+
+server "crm-vmware-3-0-001.6fusion.vodka", :app, :crm
---
+++ config/deploy/whiskey-crm.rb
@@ -0,0 +1,4 @@
+set :deploy_env, "whiskey-crm"
+set :context_path, "/vmware"
+
+server "crm-vmware-3-0-001.6fusion.whiskey", :app, :crm
