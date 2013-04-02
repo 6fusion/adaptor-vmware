bcec57d3305a5f55e188cf5b860c86d2eccfb9c0
Alex Gandy <alexgandy@gmail.com>
2013-04-02 14:16:15 -0400

    First attempt at letting the hypervisor parse OVF XML, and return as much information as possible.

--- app/views/media/base.rabl
+++ app/views/media/base.rabl
@@ -1,1 +1,26 @@
 # TODO: Figure out what the response looks like
+attributes :annotation,
+    :approximate_download_size,
+    :approximate_flat_deployment_size,
+    :approximate_sparse_deployment_size,
+    :default_deployment_option,
+    :name
+
+node :network_interfaces do
+	@medium.network_interfaces.map { |e| { :name => e[:name], :description => e[:description] } }
+end
+
+node :ip_protocols do
+	@medium.ip_protocols.map { |e| e[:assignment] }
+end
+node :ip_allocation_scheme do
+	@medium.ip_allocation_scheme.map { |e| e[:allocation_scheme] }
+end
+
+node :warnings do
+	@medium.warnings.map { |e| e[:description] }
+end
+
+node :errors do
+	@medium.errors.map { |e| e[:description] }
+end
--- models/base/medium.rb
+++ models/base/medium.rb
@@ -2,9 +2,21 @@
 # This class file should not be modified if you don't understand what you are doing.
 class Base::Medium < Main
   # TODO: Figure out what needs to go here
-  # attr_accessor :unknown
+  attr_accessor :annotation,
+    :approximate_download_size,
+    :approximate_flat_deployment_size,
+    :approximate_sparse_deployment_size,
+    :default_deployment_option,
+    :errors,
+    :name,
+    :network_interfaces,
+    :ovf_file_location,
+    :warnings,
+    :ip_allocation_scheme,
+    :ip_protocols
 
   def self.parse_ovf(inode, _ovf_location)
+    logger.info("medium.parse_ovf")
     adaptor = inode.vmware_api_adaptor
     ovf_manager = adaptor.connection.get_ovf_manager
 
@@ -17,17 +29,43 @@ class Base::Medium < Main
     parse_params.set_deployment_option("")
     ovf_parse_result = ovf_manager.parseDescriptor(ovf_xml, parse_params)
 
-    ovf_obj = {}
+    ovf_import_result = {
+        ovf_file_location: _ovf_location,
+        annotation: ovf_parse_result.get_annotation,
+        name: ovf_parse_result.get_default_entity_name,
+        approximate_download_size: ovf_parse_result.get_approximate_download_size,
+        approximate_flat_deployment_size: ovf_parse_result.get_approximate_flat_deployment_size,
+        approximate_sparse_deployment_size: ovf_parse_result.get_approximate_sparse_deployment_size,
+        default_deployment_option: ovf_parse_result.get_default_deployment_option
+    }
+
+    # ovf_parse_result.get_annotated_ost is worthless, don't explore it...seriously, i warned you -ag
+
+    # Build errors and warnings
+    ovf_import_result[:errors] = (ovf_parse_result.get_error ? ovf_parse_result.get_error.map { |e| { description: e.get_localized_message } } : {})
+    ovf_import_result[:warnings] = (ovf_parse_result.get_warning ? ovf_parse_result.get_warning.map { |w| { description: w.get_localized_message } } : {})
+
     # figure out nic details
-    ovf_obj[:nic_attributes] = ovf_parse_result.get_network.collect { |nic| {name: nic.get_name, description: nic.get_description} }
+    ovf_import_result[:network_interfaces] = (ovf_parse_result.get_network ? ovf_parse_result.get_network.collect { |nic| {name: nic.get_name, description: nic.get_description} } : {})
+    ovf_import_result[:ip_protocols] = (ovf_parse_result.get_ip_protocols ? ovf_parse_result.get_ip_protocols.collect { |na| { assignment: na } } : {})
+    ovf_import_result[:ip_allocation_scheme] = (ovf_parse_result.get_ip_allocation_scheme ? ovf_parse_result.get_ip_allocation_scheme.collect { |scheme| { allocation_scheme: scheme } } : {})
 
     # TODO: figure out disk details
-    logger.info(ovf_obj.inspect)
-
-    # TODO: figure out memory defailts
-
+    # TODO: figure out memory defaults
     # TODO: Figure out what the return object looks like
-    # return self.new({ local_path: _local_mount_path, remote_path: _remote_mount_path })
-    ovf_obj
+    self.new(ovf_import_result)
+  end
+
+  private
+  def self.log_available_methods(_object, _regex=nil, _execute_it=false)
+    logger.info("_object class type: #{_object.get_class}")
+
+    methods = _object.methods
+    methods = methods.grep(_regex) if _regex.present?
+
+    methods.each do |method|
+      logger.info "Method: #{method}"
+      logger.info "*** Result: #{_object.send(method)}" if _execute_it
+    end
   end
 end
