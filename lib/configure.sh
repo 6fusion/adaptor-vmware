f09995a5243e95e1b830bbbf36c4622fd8395683
Kevin Olbrich <kolbrich@6fusion.com>
2013-05-09 00:03:13 -0400

    bit more robust version detection

--- lib/configure.sh
+++ lib/configure.sh
@@ -11,7 +11,7 @@ function control_c {
 
 export LANG=en_US.UTF-8
 
-VERSION=$(cat /var/6fusion/adaptor-vmware/current/VERSION)
+VERSION=$(head -n1 /var/6fusion/adaptor-vmware/current/VERSION)
 
 choice=0
 while [ $choice != "quit" ]
