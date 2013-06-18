5ed5d10b29dd6e16e07aba699d191bc5a31f8eaa
satkinson138 <satkinson1122@gmail.com>
2013-06-18 15:56:42 -0400

    Tests for accesing machine inodes, machines and machine readings for the vmware adaptor.

--- features/Adapters.feature
+++ features/Adapters.feature
@@ -4,10 +4,12 @@ Feature: Adapters
   As a user of adapters
   I want to verify that I can send and receive information on a particular adapter
 
-  Background: #tbd
-
   @smoke_adapters @get_list_of_machines
 
   Scenario: I want to verify that I can get a list of machines from the xen adapter.
-    Given I get a list of machines from the existing xen adapter.
-    Then the list of machines is received
+    When I get a list of inodes from an existing xen adapter
+    When I get a list of machines from an existing xen adapter
+    When I get readings of an existing machine from an existing xen adapter
+    When I get a list of inodes from an existing vmware adapter
+    When I get a list of machines from an existing vmware adapter
+    When I get readings of an existing machine from an existing vmware adapter
--- features/step_definitions/Adapters_steps.rb
+++ features/step_definitions/Adapters_steps.rb
@@ -1,9 +1,40 @@
+$xenTestVariables = {:inode_uuid => "", :machine_uuid => "", :readings => ""}
+$vmwareTestVariables = {:inode_uuid => "", :machine_uuid => "", :readings => ""}
 
-## Get a list of machines in the existing xen adapter.
-Given(/^I get a list of machines from the existing xen adapter\.$/) do
-    response                = JSON.parse(RestClient.get ":8080/inodes/", :content_type => :json, :accept => :json)
+## Get a list of inodes in the existing xen adapter.
+Given(/^I get a list of inodes from an existing xen adapter$/) do
+    response                = JSON.parse(RestClient.get "http://adaptor-xen-1-0-001.6fusion.vodka:8080/inodes/", :content_type => :json, :accept => :json)
+    $xenTestVariables[:inode_uuid] = response[0]["uuid"]
+    #print("#{$xenTestVariables[:inode_uuid]}\n\n\n")
 end
 
-Then(/^the list of machines is received$/) do
-    pending # express the regexp above with the code you wish you had
+## Get a list of machines in the existing xen adapter.
+Given(/^I get a list of machines from an existing xen adapter$/) do
+    response                = JSON.parse(RestClient.get "http://adaptor-xen-1-0-001.6fusion.vodka:8080/inodes/#{$xenTestVariables[:inode_uuid]}/machines/", :content_type => :json, :accept => :json)
+    $xenTestVariables[:machine_uuid] = response[0]["uuid"]
+    #print("#{$xenTestVariables[:machine_uuid]}\n\n\n")
+end
+
+## Get a list of machines in the existing xen adapter.
+Given(/^I get readings of an existing machine from an existing xen adapter$/) do
+    response                = JSON.parse(RestClient.get "http://adaptor-xen-1-0-001.6fusion.vodka:8080/inodes/#{$xenTestVariables[:inode_uuid]}/machines/#{$xenTestVariables[:machine_uuid]}/readings", :content_type => :json, :accept => :json)
+end
+
+## Get a list of inodes in the existing vmware adapter.
+Given(/^I get a list of inodes from an existing vmware adapter$/) do
+    response                = JSON.parse(RestClient.get "http://adaptor-vmware-1-0-001.6fusion.vodka:8080/inodes/", :content_type => :json, :accept => :json)
+    $vmwareTestVariables[:inode_uuid] = response[0]["uuid"]
+    #print("#{$vmwareTestVariables[:inode_uuid]}\n\n\n")
+end
+
+## Get a list of machines in the existing vmware adapter.
+Given(/^I get a list of machines from an existing vmware adapter$/) do
+    response                = JSON.parse(RestClient.get "http://adaptor-vmware-1-0-001.6fusion.vodka:8080/inodes/#{$vmwareTestVariables[:inode_uuid]}/machines/", :content_type => :json, :accept => :json)
+    $vmwareTestVariables[:machine_uuid] = response[0]["uuid"]
+    #print("#{$vmwareTestVariables[:machine_uuid]}\n\n\n")
+end
+
+## Get a list of machines in the existing vmware adapter.
+Given(/^I get readings of an existing machine from an existing vmware adapter$/) do
+    response                = JSON.parse(RestClient.get "http://adaptor-vmware-1-0-001.6fusion.vodka:8080/inodes/#{$vmwareTestVariables[:inode_uuid]}/machines/#{$vmwareTestVariables[:machine_uuid]}/readings", :content_type => :json, :accept => :json)
 end
