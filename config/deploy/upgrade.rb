59cf403a256b4bab1d52a0eb84ae32ae507da649
Kevin Olbrich <kolbrich@6fusion.com>
2013-08-17 16:41:34 -0400

    some formatting changes and make sure that capistrano does not attempt to restart torquebox during an upgrade so that the master install script can handle that later

--- config/deploy/upgrade.rb
+++ config/deploy/upgrade.rb
@@ -1,12 +1,12 @@
-set :context_path, ENV['CONTEXT_PATH'] || "/vmware"
+set :context_path, ENV['CONTEXT_PATH'] || '/vmware'
 set :hipchat_alert, false
-set :repository, "."
+set :repository, '.'
 set :deploy_via, :copy
 set :copy_dir, '/tmp/upgrade/capistrano/adaptor-vmware-copy'
 set :scm, :none
 set :rails_env, ENV['RAILS_ENV'] || 'production'
 set :bundle_flags, '--local --deployment --quiet'
-set :bundle_cmd, "jruby -S bundle"
+set :bundle_cmd, 'jruby -S bundle'
 set :use_sudo, false
 
 def local_run(cmd, options = {})
@@ -23,7 +23,7 @@ end
 
 alias :capture :local_capture
 
-server "127.0.0.1", :app, :web, primary: true
+server '127.0.0.1', :app, :web, primary: true
 
 before 'deploy' do
   run "#{sudo} mkdir -p #{copy_dir}"
@@ -43,7 +43,7 @@ end
 
 namespace :deploy do
   task :check_specs do
-    # do nothing, don't check the specs when deploying to development
+    # do nothing, don't check the specs when upgrading
   end
 
   task :write_version_file do
@@ -54,6 +54,10 @@ namespace :deploy do
 
   task :update_code do
     run "cp -R . #{release_path}"
+  end
+
+  task :restart do
+    # don't do anything here, usually a deploy will result in a torquebox restart but that gets handled by the master install script when upgrading.
   end
 end
 
